98916acb5aa5fd9284d2bd4499f5accde0afc67f
MR <maximencia@yandex.ru>
2016-11-27 00:29:58 +0300

    Запуск Firefox
            #firefox_driver = webdriver.Firefox(firefox_binary="c:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe")
            #firefox_driver = webdriver.Firefox(firefox_binary="c:\\Program Files (x86)\\Mozilla_Firefox_ESR\\firefox.exe")
    
            #firefox_driver= webdriver.Firefox(capabilities={"marionette": True})
            # # старая схема:
            firefox_driver = webdriver.Firefox(capabilities={"marionette": False})
    
    Алексей, приветствую. Не могу понять. Установлены 2 версии FF, если указывать путь до браузера то запускается указанный. Если играться с  capabilities то запускается только ESR.

--- lc_admin_login.py
+++ lc_admin_login.py
@@ -18,19 +18,40 @@ def is_alert_present(wd):
 
 class test_admin_login(unittest.TestCase):
     def setUp(self):
-        chrome_driver = webdriver.Chrome #(desired_capabilities={"chromeOptions": {"args": ["--start-fullscreen"]}})
 
-        options = Options()
-        options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
-        #options.add_argument("start-maximized")
-        options.add_argument("--start-fullscreen")
+#chrome
+        ##chrome_driver = webdriver.Chrome #(desired_capabilities={"chromeOptions": {"args": ["--start-fullscreen"]}})
+        #options = Options()
+        #options.binary_location = "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+        ##options.add_argument("start-maximized")
+        #options.add_argument("--start-fullscreen")
 
+#ie
         #ie_driver = webdriver.Ie()
-        #firefox_driver = webdriver.Firefox()
 
-        self.wd = chrome_driver(chrome_options=options)
+#ff
+        # простая схема, которую я использовал ранее с текущей версией FF = 50
+        #firefox_driver = webdriver.Firefox
+
+        # Попробуйте запустить разработанный ранее сценарий логина в браузере Firefox ESR 45,
+        # используя старую схему запуска, без использования geckodriver.
+        # Если Selenium не может сам найти место, куда установлен Firefox ESR -- укажите в параметрах запуска путь к браузеру.
+
+        firefox_driver = webdriver.Firefox(firefox_binary="c:\\Program Files (x86)\\Mozilla Firefox\\firefox.exe")
+        # firefox_driver = webdriver.Firefox(firefox_binary="c:\\Program Files (x86)\\Mozilla_Firefox_ESR\\firefox.exe")
+
+        # # новая схема:
+        # from selenium import webdriver
+        #
+        # wd = webdriver.Firefox()
+        # # новая схема более явно:
+        # firefox_driver= webdriver.Firefox(capabilities={"marionette": True})
+        # # старая схема:
+        # firefox_driver = webdriver.Firefox(capabilities={"marionette": False})
+        #
+        #self.wd = chrome_driver(chrome_options=options)
         #self.wd = ie_driver
-        #self.wd = firefox_driver
+        self.wd = firefox_driver
         #self.wd = webdriver()
         self.wd.implicitly_wait(60)
 
@@ -49,9 +70,9 @@ class test_admin_login(unittest.TestCase):
         self.find_and_fill_element(wd,element_name="username",value="admin")
         self.find_and_fill_element(wd,element_name="password",value="admin")
         wd.find_element_by_name("login").click()
-        time.sleep(3)
-        wd.find_element_by_link_text("Appearence").click()
-        wd.find_element_by_link_text("Catalog").click()
+        time.sleep(5)
+        #wd.find_element_by_link_text("Appearence").click()
+        #wd.find_element_by_link_text("Catalog").click()
         # wd.find_element_by_link_text("Product Groups").click()
         # wd.find_element_by_link_text("Option Groups").click()
         # wd.find_element_by_link_text("Manufacturers").click()
