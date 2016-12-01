67b1e5805c54e19611d8d7396991631f6e285d0a
maxim.rumyantsev <maxim.rumyantsev@billing.ru>
2016-12-02 02:26:21 +0300

    пробегаем по меню и считаем подпункты

--- lc_admin_login.py
+++ lc_admin_login.py
@@ -33,19 +33,28 @@ def test_admin_login(wd):
     wd.find_element_by_name("login").click()
     #sleep(5)
     #p = wd.find_elements_by_xpath("//ul[@id='box-apps-menu']")
+
+    #читаем основное меню
     l=wd.find_elements_by_xpath("//*[@id='app-']/a/span[2]")
-    for i in range(len(l)-15):
-        print (str(i)+".")
+    for i in range(len(l)):
+        print
+        #print (str(i)+".")
         t=wd.find_elements_by_xpath("//*[@id='app-']/a/span[2]")
         link_name=t[i].text
+        print (str(i)+"."+link_name)
+        #переходим к пункту основного меню
         t[i].click()
+        #ищем пункты подменю
+        wd.implicitly_wait(2)
+        list_sub_menu=wd.find_elements_by_xpath("//ul[@class='docs']//li//span")
+        print ("   "+str(len(list_sub_menu)))
 
-        print link_name
+        #print link_name
         #а заголовок в шапке можно получить так: driver.getTitle()
-        tt=wd.title
-        ttt=wd.find_element_by_xpath(".//*[@id='content']/h1").text
-        print tt
-        print ttt
+        #tt=wd.title
+        #ttt=wd.find_element_by_xpath(".//*[@id='content']/h1").text
+        #print tt
+        #print ttt
 
         #ttt=wd.find_element_by_xpath("//title").text
         #print wd.find_element_by_xpath("//title").text
