7609e9c68bf3300d1fac852192c77842b55dc931
maxim.rumyantsev <maxim.rumyantsev@billing.ru>
2016-12-02 17:33:36 +0300

    Задание 7. Сделайте сценарий, проходящий по всем разделам админки ГОТОВО
    Сделайте сценарий, который выполняет следующие действия в учебном приложении litecart.
    
    1) входит в панель администратора http://localhost/litecart/admin
    2) прокликивает последовательно все пункты меню слева, включая вложенные пункты
    3) для каждой страницы проверяет наличие заголовка
    
    Немного переборщил и сделал сложнее  надеюсь на премию.

--- lc_admin_login.py
+++ lc_admin_login.py
@@ -31,31 +31,46 @@ def test_admin_login(wd):
     find_and_fill_element(wd,element_name="username",value="admin")
     find_and_fill_element(wd,element_name="password",value="admin")
     wd.find_element_by_name("login").click()
+    print
     #sleep(5)
     #p = wd.find_elements_by_xpath("//ul[@id='box-apps-menu']")
-
+    link_array=[]
+    h1_array=[]
     #читаем основное меню
-    l=wd.find_elements_by_xpath("//*[@id='app-']/a/span[2]")
-    for i in range(len(l)):
-        print
-        #print (str(i)+".")
-        t=wd.find_elements_by_xpath("//*[@id='app-']/a/span[2]")
-        link_name=t[i].text
+    mm=wd.find_elements_by_xpath("//*[@id='app-']/a/span[2]") # количество пунктов
+    for i in range(len(mm)):
+        # как только мы кликаем по ссылке - страница меняется и то что было в mm_l становится не актуальным
+        #  - так что ищем список заного и выбираем нужный элемент по индексу.
+        mm_el=wd.find_elements_by_xpath("//*[@id='app-']/a/span[2]")
+        link_name=mm_el[i].text # имя пункта главного меню
         print (str(i)+"."+link_name)
         #переходим к пункту основного меню
-        t[i].click()
+        mm_el[i].click()
+
         #ищем пункты подменю
-        wd.implicitly_wait(2)
-        list_sub_menu=wd.find_elements_by_xpath("//ul[@class='docs']//li//span")
-        print ("   "+str(len(list_sub_menu)))
+        wd.implicitly_wait(1)
+        sub_menu=wd.find_elements_by_xpath("//ul[@class='docs']//li//span")
+        print ("   Total subsmenu elements:"+str(len(sub_menu)))
 
-        #print link_name
-        #а заголовок в шапке можно получить так: driver.getTitle()
-        #tt=wd.title
-        #ttt=wd.find_element_by_xpath(".//*[@id='content']/h1").text
-        #print tt
-        #print ttt
-
-        #ttt=wd.find_element_by_xpath("//title").text
-        #print wd.find_element_by_xpath("//title").text
-        # Максим Максим http://tonyganch.com/git/reset/
+        if len(sub_menu)<1:
+            head_title=wd.find_element_by_xpath(".//*[@id='content']/h1").text
+            print("Main Link_text  : "+link_name)
+            print("     Head_title : "+head_title)
+            link_array.append(link_name)
+            h1_array.append(head_title)
+        for j in range(len(sub_menu)):
+            sub_menu_el=wd.find_elements_by_xpath("//ul[@class='docs']//li//span")
+            sub_menu_el[j].click()
+            # эх - после клика нужно все перечитывать!
+            #найдем пункт меню и h1
+            sub_menu_el=wd.find_elements_by_xpath("//ul[@class='docs']//li//span")
+            sub_m_link_name=sub_menu_el[j].text # имя пункта главного меню
+            head_title2=wd.find_element_by_xpath(".//*[@id='content']/h1").text
+            print("     Link_text  : "+sub_m_link_name)
+            print("     Head_title : "+head_title2)
+            link_array.append(sub_m_link_name)
+            h1_array.append(head_title2)
+            print
+    print (link_array)
+    print (h1_array)
+    assert (link_array)==(h1_array)
---
+++ sendbox.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+a = 3
+b = 5
+r = 0  # Чтобы было, чем заполнять
+mas = []
+for i in range(a):
+    mas.append([])
+    for j in range(b):
+        mas[i].append(r)
+        r += 1  # Чтобы заполнялось не одно и тоже
+
+print(mas)
+print ((mas[0][2    ]))
